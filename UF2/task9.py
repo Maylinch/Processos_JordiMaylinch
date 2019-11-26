d4fda5855d1a11fe7872ce11cc0739e2c921968d
Maylinch <55596248+Maylinch@users.noreply.github.com>
2019-11-26 18:25:24 +0100

    Add files via upload

---
+++ UF2/task9.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from multiprocessing import Pool
+
+def primers(num):
+    for i in range(2, num/2):
+        if num % i == 0:
+            return False
+        else:
+            pass
+    return True
+
+if __name__ == '__main__':
+#El Pool ens servira per indicar quants processos volem executar a la vegada.
+    pool = Pool(processes=10)
+    l = range(40000000, 40000100)
+#Aquesta linia inicia el contador per poder saber quan tarda.
+    start = datetime.now()
+    it = pool.imap(primers, l)
+    i = 0
+    m = 40000000
+
+#El valor I defineix quants numeros volem mostrar
+
+    while(i<100):
+        print str(m), str(it.next())
+        m+=1
+        i+=1
+#Aquesta ultima linia es la que mostrara el temps que ha tardat
+print datetime.now() - start
